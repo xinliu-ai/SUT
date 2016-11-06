df9894ec8d1baafc809ce7070f3378748b9299ba
xinliu-ai <xinliu-ai@gmail.com>
2016-11-06 17:42:33 +0800

    add mvc

---
+++ SUT/Model/SUT.h
@@ -0,0 +1,13 @@
+//
+//  SUT.h
+//  SUT
+//
+//  Created by xin on 16/11/6.
+//  Copyright © 2016年 xin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SUT : NSObject
+
+@end
---
+++ SUT/Model/SUT.m
@@ -0,0 +1,13 @@
+//
+//  SUT.m
+//  SUT
+//
+//  Created by xin on 16/11/6.
+//  Copyright © 2016年 xin. All rights reserved.
+//
+
+#import "SUT.h"
+
+@implementation SUT
+
+@end
