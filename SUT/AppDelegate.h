8ae7707ea75ee84adf7689544ea89b34b9f1b9ad
xinliu-ai <xinliu-ai@gmail.com>
2016-11-06 17:38:12 +0800

    setup project

---
+++ SUT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SUT
+//
+//  Created by xin on 16/11/6.
+//  Copyright © 2016年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SUT/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SUT
+//
+//  Created by xin on 16/11/6.
+//  Copyright © 2016年 xin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
